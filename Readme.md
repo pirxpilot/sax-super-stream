96fe12f967b54171a995599cc4abb302b074c780
Damian Krzeminski <pirxpilot@code42day.com>
2016-12-01 20:30:30 -0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,34 @@
+[![NPM version][npm-image]][npm-url]
+[![Build Status][travis-image]][travis-url]
+[![Dependency Status][gemnasium-image]][gemnasium-url]
+
+# sax-super-stream
+
+Transform stream implemented using SAX with hierarchical parsing
+
+## Install
+
+```sh
+$ npm install --save sax-super-stream
+```
+
+## Usage
+
+```js
+var saxSuperStream = require('sax-super-stream');
+
+saxSuperStream('Rainbow');
+```
+
+## License
+
+MIT © [Damian Krzeminski](https://code42day.com)
+
+[npm-image]: https://img.shields.io/npm/v/sax-super-stream.svg
+[npm-url]: https://npmjs.org/package/sax-super-stream
+
+[travis-url]: https://travis-ci.org/code42day/sax-super-stream
+[travis-image]: https://img.shields.io/travis/code42day/sax-super-stream.svg
+
+[gemnasium-image]: https://img.shields.io/gemnasium/code42day/sax-super-stream.svg
+[gemnasium-url]: https://gemnasium.com/code42day/sax-super-stream
---
+++ test/stream.js
@@ -0,0 +1,118 @@
+var fs = require('fs');
+var Writable = require('stream').Writable;
+
+var stream = require('..');
+
+/*global describe, it */
+
+
+function readStream(name) {
+  return fs.createReadStream([__dirname, 'fixtures', name].join('/'));
+}
+
+function memory(array) {
+  return new Writable({
+    objectMode: true,
+    write: function (item, encoding, next) {
+      array.push(item);
+      next();
+    }
+  });
+}
+
+describe('sax super stream', function(){
+  it('should parse a single empty node', function(done){
+    var config = {
+      'item': function() { return {}; }
+    };
+    var result = [];
+
+    function verify() {
+      result.should.have.length(1);
+      result[0].should.eql({});
+      done();
+    }
+
+    readStream('one.xml')
+      .pipe(stream(config))
+      .pipe(memory(result))
+      .on('finish', verify)
+      .on('error', done);
+  });
+
+  it('should parse nodes with text', function(done){
+    var config = {
+      'two': {
+        'item': {
+          $: function() { return {}; },
+          'a': { $text: stream.assignTo('A') },
+          'b': { $text: stream.assignTo('B') }
+        }
+      }
+    };
+    var result = [];
+
+    function verify() {
+      result.should.have.length(2);
+      result[0].should.have.property('A', 'abc');
+      result[0].should.have.property('B', '15');
+      result[1].should.have.property('A', 'def');
+      result[1].should.have.property('B', '16');
+      done();
+    }
+
+    readStream('two.xml')
+      .pipe(stream(config))
+      .pipe(memory(result))
+      .on('finish', verify)
+      .on('error', done);
+  });
+
+  it('should parse attributes', function(done){
+    var config = {
+      'THREE': {
+        'ITEMS': {
+          'ITEM': {
+            $: function() { return {}; },
+            'A': { $: appendToCollection },
+            'B': { $: addToParent }
+          }
+        }
+      }
+    };
+    var result = [];
+
+    function appendToCollection(node, parent) {
+      var obj = {
+        value: node.attributes.attr.value
+      };
+      parent.children = parent.children || [];
+      parent.children.push(obj);
+    }
+
+    function addToParent(node, parent) {
+      parent.b = node.attributes.attr.value;
+    }
+
+    function verify() {
+      var item, a;
+
+      result.should.have.length(1);
+
+      item = result[0];
+      item.should.have.property('b', '4');
+
+      a = item.children;
+      a.should.have.length(3);
+      a[0].should.have.property('value', '1');
+      done();
+    }
+
+    readStream('three.xml')
+      .pipe(stream(config, { lowercase: true }))
+      .pipe(memory(result))
+      .on('finish', verify)
+      .on('error', done);
+  });
+
+});
