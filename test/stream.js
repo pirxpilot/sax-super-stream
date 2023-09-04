88f0659d9e6ad6f2b52dd3924e6ac02d27a8f3e9
Damian Krzeminski <pirxpilot@furkot.com>
2023-09-04 13:03:46 +0200

    rewrite using web TransformStream and Saxophone
    please note that TextDecoderStream needs to be used on binary streams
    
    this version is using web streams instead of node streams
    
    using TransformStream allows direct usage in web browser
    using in node is also possible since node's pipe accepts web streams

--- test/stream.js
+++ test/stream.js
@@ -1,228 +1,285 @@
-const { describe, it } = require('node:test');
+const test = require('node:test');
+const assert = require('node:assert/strict');
 const fs = require('node:fs');
+const path = require('node:path');
 const { pipeline } = require('node:stream/promises');
-const { Writable, Readable } = require('stream');
 
 const stream = require('..');
 
 function readStream(name) {
-  return fs.createReadStream([__dirname, 'fixtures', name].join('/'));
+  return fs.createReadStream(path.join(__dirname, 'fixtures', name));
 }
+
+/* global WritableStream, ReadableStream, TextDecoderStream */
 
 function memory(array) {
-  return new Writable({
-    objectMode: true,
-    write(item, encoding, next) {
-      array.push(item);
-      next();
-    }
+  return new WritableStream({
+    write: item => array.push(item)
   });
 }
 
-describe('sax super stream', () => {
-  it('should parse a single empty node', async () => {
-    const config = {
-      'item': stream.object()
-    };
-    const result = [];
+test('should parse a single empty node', async () => {
+  const config = {
+    'item': stream.object()
+  };
+  const result = [];
 
-    await pipeline(
-      readStream('one.xml'),
-      stream(config),
-      memory(result)
-    );
-    result.should.have.length(1);
-    result[0].should.eql({});
+  await pipeline(
+    readStream('one.xml'),
+    new TextDecoderStream(),
+    stream(config),
+    memory(result)
+  );
 
-  });
+  assert.deepEqual(result, [{}]);
+});
 
-  it('should parse nodes with text', async () => {
-    const config = {
-      'two': {
-        'item': {
-          $: stream.object(),
-          'a': { $text: stream.assignTo('A') },
-          'b': { $text: stream.assignTo('B') }
-        }
+test('should parse nodes with text', async () => {
+  const config = {
+    'two': {
+      'item': {
+        $: stream.object(),
+        'a': { $text: stream.assignTo('A') },
+        'b': { $text: stream.assignTo('B') }
       }
-    };
-    const result = [];
-
-    await pipeline(
-      readStream('two.xml'),
-      stream(config),
-      memory(result)
-    );
-    result.should.have.length(2);
-    result[0].should.have.property('A', 'abc');
-    result[0].should.have.property('B', '15');
-    result[1].should.have.property('A', 'def');
-    result[1].should.have.property('B', '16');
-
-
-  });
-
-  it('should parse attributes', async () => {
-    const config = {
-      'THREE': {
-        'ITEMS': {
-          'ITEM': {
-            $: stream.object(),
-            'A': { $: appendToCollection },
-            'B': { $: addToParent }
-          }
-        }
-      }
-    };
-    const result = [];
-
-    function appendToCollection({ attributes }, parent) {
-      const obj = {
-        value: attributes.attr.value
-      };
-      parent.children = parent.children || [];
-      parent.children.push(obj);
     }
+  };
+  const result = [];
 
-    function addToParent({ attributes }, parent) {
-      parent.b = attributes.attr.value;
+  await pipeline(
+    readStream('two.xml'),
+    new TextDecoderStream(),
+    stream(config),
+    memory(result)
+  );
+  assert.deepEqual(result, [
+    { A: 'abc', B: '15' },
+    { A: 'def', B: '16' }
+  ]);
+});
+
+test('should parse nested nodes', async () => {
+  const itemParser = {
+    $: (_, parent) => parent.item = {},
+    a: { $text: stream.assignTo('A') }
+  };
+  itemParser.item = itemParser;
+  const config = {
+    nested: {
+      $: stream.object(),
+      item: itemParser
     }
+  };
+  const result = [];
 
-    await pipeline(
-      readStream('three.xml'),
-      stream(config, { lowercase: true }),
-      memory(result)
-    );
-    let item;
-    let a;
-
-    result.should.have.length(1);
-
-    item = result[0];
-    item.should.have.property('b', '4');
-
-    a = item.children;
-    a.should.have.length(3);
-    a[0].should.have.property('value', '1');
-
-  });
-
-  it('should call $after parser if specified', async () => {
-    let value = 0;
-    const config = {
-      'doc': {
-        'item': {
-          $: stream.object(),
-          $after(obj) { obj.value = value++; }
+  await pipeline(
+    readStream('nested.xml'),
+    new TextDecoderStream(),
+    stream(config),
+    memory(result)
+  );
+  assert.deepEqual(result, [{
+    item: {
+      A: 'abc',
+      item: {
+        A: 'def',
+        item: {
+          A: 'ghi',
         }
       }
-    };
-    const result = [];
+    }
+  }]);
+});
 
-    await pipeline(
-      readStream('ns.xml'),
-      stream(config),
-      memory(result)
-    );
-
-    value.should.be.eql(2);
-    result.should.have.length(2);
-    result[0].should.have.property('value', 0);
-    result[1].should.have.property('value', 1);
-
-  });
-
-  it('should ignore namespace if none declared', async () => {
-    const config = {
-      'doc': {
-        'item': { $: stream.object() }
-      }
-    };
-    const result = [];
-
-    await pipeline(
-      readStream('ns.xml'),
-      stream(config),
-      memory(result)
-    );
-
-    result.should.have.length(2);
-  });
-
-  it('should accept elements if namespace matches $uri attribute', async () => {
-    const config = {
-      'doc': {
-        $uri: 'http://example.com',
-        'item': { $: stream.object() }
-      }
-    };
-    const result = [];
-
-    await pipeline(
-      readStream('ns.xml'),
-      stream(config),
-      memory(result));
-    result.should.have.length(2);
-
-  });
-
-  it('should ignore elements if namespace does not match $uri attribute', async () => {
-    const config = {
-      'doc': {
-        $uri: 'http://another.com',
-        'item': { $: stream.object() }
-      }
-    };
-    const result = [];
-
-    await pipeline(
-      readStream('ns.xml'),
-      stream(config),
-      memory(result));
-
-    result.should.have.length(0);
-  });
-
-
-  it('should parse CDATA as text', async () => {
-    const config = {
-      'FOUR': {
+test('should parse attributes', async () => {
+  const config = {
+    'THREE': {
+      'ITEMS': {
         'ITEM': {
           $: stream.object(),
-          'A': { $text: stream.assignTo('a') },
-          'B': { $text: stream.assignTo('b') }
+          'A': { $: appendToCollection },
+          'B': { $: addToParent }
         }
       }
+    }
+  };
+  const result = [];
+
+  function appendToCollection({ attributes }, parent) {
+    const obj = {
+      value: attributes.attr.value
     };
-    const result = [];
+    parent.children = parent.children || [];
+    parent.children.push(obj);
+  }
 
-    await pipeline(
-      readStream('four.xml'),
-      stream(config),
-      memory(result));
+  function addToParent({ attributes }, parent) {
+    parent.b = attributes.attr.value;
+  }
 
-    result.should.have.length(2);
-    result[0].should.be.eql({ a: 'abc', b: '15' });
-    result[1].should.be.eql({ a: 'def', b: '16' });
+  await pipeline(
+    readStream('three.xml'),
+    new TextDecoderStream(),
+    stream(config, { lowercase: true }),
+    memory(result)
+  );
 
-  });
+  assert.deepEqual(result, [{
+    b: '4',
+    children: [
+      { value: '1' },
+      { value: '2' },
+      { value: '3' }
+    ]
+  }]);
+});
 
-  it('should raise errors on invalid XML', async () => {
+test('should call $after parser if specified', async () => {
+  let value = 0;
+  const config = {
+    'doc': {
+      'item': {
+        $: stream.object(),
+        $after(obj) { obj.value = value++; }
+      }
+    }
+  };
+  const result = [];
 
-    const config = {
+  await pipeline(
+    readStream('ns.xml'),
+    new TextDecoderStream(),
+    stream(config),
+    memory(result)
+  );
+
+  assert.equal(value, 2);
+  assert.deepEqual(result, [
+    { value: 0 },
+    { value: 1 }
+  ]);
+});
+
+test('should ignore namespace if none declared', async () => {
+  const config = {
+    'doc': {
       'item': { $: stream.object() }
-    };
+    }
+  };
+  const result = [];
 
-    const from = new Readable({
-      read() {}
-    });
+  await pipeline(
+    readStream('ns.xml'),
+    new TextDecoderStream(),
+    stream(config),
+    memory(result)
+  );
 
-    const pipe = pipeline(from, stream(config));
+  assert.deepEqual(result, [{}, {}]);
+});
 
-    from.push('<item>');
-    from.push('</not-item>');
-    from.push(null);
+test('should accept elements if namespace matches $uri attribute', async () => {
+  const config = {
+    'doc': {
+      $uri: 'http://example.com',
+      'item': { $: stream.object() }
+    }
+  };
+  const result = [];
 
-    await pipe.should.be.rejectedWith('Unexpected close tag');
-  });
+  await pipeline(
+    readStream('ns.xml'),
+    new TextDecoderStream(),
+    stream(config),
+    memory(result)
+  );
+  assert.deepEqual(result, [{}, {}]);
+});
+
+test('should ignore elements if namespace does not match $uri attribute', async () => {
+  const config = {
+    'doc': {
+      $uri: 'http://another.com',
+      'item': { $: stream.object() }
+    }
+  };
+  const result = [];
+
+  await pipeline(
+    readStream('ns.xml'),
+    new TextDecoderStream(),
+    stream(config),
+    memory(result)
+  );
+  assert.deepEqual(result, []);
+});
+
+
+test('should parse CDATA as text', async () => {
+  const config = {
+    'FOUR': {
+      'ITEM': {
+        $: stream.object(),
+        'A': { $text: stream.assignTo('a') },
+        'B': { $text: stream.assignTo('b') }
+      }
+    }
+  };
+  const result = [];
+
+  await pipeline(
+    readStream('four.xml'),
+    new TextDecoderStream(),
+    stream(config),
+    memory(result)
+  );
+
+  assert.deepEqual(result, [
+    { a: 'abc', b: '15' },
+    { a: 'def', b: '16' }
+  ]);
+});
+
+test('should parse entities in text but not in CDATA', async () => {
+  const config = {
+    doc: {
+      item: {
+        $: stream.object(),
+        $text: stream.assignTo('a'),
+      }
+    }
+  };
+  const result = [];
+
+  const from = ReadableStream.from([
+    '<doc>',
+    '<item>1&lt;2</item>',
+    '<item><![CDATA[1&2]]></item>',
+    '</doc>'
+  ]);
+
+  await pipeline(
+    from,
+    stream(config),
+    memory(result)
+  );
+
+  assert.deepEqual(result, [
+    { a: '1<2' },
+    { a: '1&2' }
+  ]);
+});
+
+test('should raise errors on invalid XML', async () => {
+  const config = {
+    'item': { $: stream.object() }
+  };
+
+  const from = ReadableStream.from([
+    '<item>',
+    '</not-item>'
+  ]);
+
+  const pipe = pipeline(from, stream(config), memory([]));
+  await assert.rejects(pipe, /unclosed tag: item/i);
 });
